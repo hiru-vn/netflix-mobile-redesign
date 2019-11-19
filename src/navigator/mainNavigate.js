d7397206e46a968aedd0273dafc413f8a60fc8d6
quanghuyUIT <quanghuy1998kh@gmail.com>
2019-11-19 09:53:00 +0700

    add screen shots

--- src/navigator/mainNavigate.js
+++ src/navigator/mainNavigate.js
@@ -56,7 +56,7 @@ const RootStack = createStackNavigator({
     }
 },  
     {
-        initialRouteName: 'Movie',
+        initialRouteName: 'ChangeAccount',
         horizontalTransitionConfig,
     }
 );
